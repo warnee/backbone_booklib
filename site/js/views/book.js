e969532d7f6aded9f436ec3ad5f1ea22fb3fdcea
TaewonJung <darkwarnee@gmail.com>
2014-08-05 15:29:46 +0900

    수정 기능 추가

--- site/js/views/book.js
+++ site/js/views/book.js
@@ -4,11 +4,36 @@ app.BookView = Backbone.View.extend({
     tagName: 'div',
     className: 'bookContainer',
     events: {
-        'click .delete': 'deleteBook'
+        'click .delete': 'deleteBook',
+        'click .edit': 'editBook',
+        'click .save': 'saveBook',
+        'click .cancel': 'cancelBook'
     },
     deleteBook: function() {
         this.model.destroy(); // 화면상의 삭제
         this.remove(); // 실제 삭제
+    },
+    editBook: function() {
+        $('.viewMode[data-id="' + this.model.id + '"]').hide();
+        $('.editMode[data-id="' + this.model.id + '"]').show();
+    },
+    saveBook: function() {
+
+        var editData = {};
+        editData.title = $('.editMode[data-id="' + this.model.id + '"] .title').val();
+        editData.author = $('.editMode[data-id="' + this.model.id + '"] .author').val();
+        editData.releaseDate = $('.editMode[data-id="' + this.model.id + '"] .releaseDate').val();
+        editData.keywords = $('.editMode[data-id="' + this.model.id + '"] .keywords').val();
+
+        this.model.save(editData); // 저장
+        this.render(); // model 재 설정
+        //this.update();
+        $('.viewMode[data-id="' + this.model.id + '"]').show();
+        $('.editMode[data-id="' + this.model.id + '"]').hide();
+    },
+    cancelBook: function() {
+        $('.viewMode[data-id="' + this.model.id + '"]').show();
+        $('.editMode[data-id="' + this.model.id + '"]').hide();
     },
     //template: _.template( $('#bookTemplate').html() ),
     template: Hogan.compile($("#bookTemplate").html()),
